e97bad5157c34c176f306d64367ebd2be219d0d2
abachi <abachinasser@gmail.com>
2022-07-21 22:17:56 +0100

    use https in production and use for seeding

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (Config::get('app.env') == 'production') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
